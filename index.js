7890504f96daa3153f59575ef51cb1f5a4b4b0c3
elani-web <akachukwumarvellous0@gmail.com>
2025-07-31 21:01:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,7 @@ app.set('view engine', 'ejs');
 
 const { Pool } = pg;
 const db = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
   max: 1,
 });
 
